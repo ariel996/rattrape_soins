22c41a28cae09569db17364fc92104dcb8a5c802
Gael <meliarchille@gmail.com>
2023-03-24 05:37:58 +0100

    Add migration, Model and Controller

---
+++ app/Http/Controllers/Api/PersonnelController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Personnel;
+use Illuminate\Http\Request;
+
+class PersonnelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Personnel  $personnel
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Personnel $personnel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Personnel  $personnel
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Personnel $personnel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Personnel  $personnel
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Personnel $personnel)
+    {
+        //
+    }
+}
--- database/migrations/2023_03_24_015637_create_roles_table.php
+++ database/migrations/2023_03_24_015637_create_roles_table.php
@@ -13,18 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('surname')->nullable();
-            $table->string('email')->unique();
-            $table->date('dob')->nullable();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-
-            $table->foreignIdFor(\App\Models\Role::class)->cascadeOnDelete();
-
             $table->timestamps();
         });
     }
@@ -36,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('roles');
     }
 };
